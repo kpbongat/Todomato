06753e83ecbc2f862d274faafc34a14380c64626
kpbongat <kpbongat.dev@gmail.com>
2024-10-29 15:17:46 +0800

    Implement Web Storage API for data persistence

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "../Header/Header";
 import TodoPanel from "../TodoPanel/TodoPanel";
 import styles from "./App.module.css";
 function App() {
   const [todoList, setTodoList] = useState([]);
-
+  useEffect(() => {
+    setTodoList(JSON.parse(localStorage.getItem("todoList")) || []);
+  }, []);
   return (
     <div className={styles.div}>
       <Header />
-      <TodoPanel todoList={todoList} setTodoList={setTodoList} />
+      <TodoPanel
+        todoList={todoList}
+        setTodoList={(state) => {
+          localStorage.setItem("todoList", JSON.stringify(state));
+          setTodoList(state);
+        }}
+      />
     </div>
   );
 }
